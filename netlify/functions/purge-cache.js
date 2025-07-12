a3becc5ea8a1dd51b9ae44f7cc4e741f354426e8
Heidintt <dkdaisuki2@gmail.com>
2025-07-12 20:32:24 +1000

    Create purge-cache.js

---
+++ netlify/functions/purge-cache.js
@@ -0,0 +1,7 @@
+import { purgeCache } from "@netlify/functions";
+
+export default async () => {
+  console.log("Purging everything");
+  await purgeCache();
+  return new Response("Purged!", { status: 202 });
+};
